4a32e2ecf141eace031971a72000a41d5e469d84
luiz.sousa.fagron <luiz.sousa@fagrontech.com.br>
2020-03-06 17:06:36 -0300

    initial commit

---
+++ HealthyDashboard/HealthyDashboard/Startup.cs
@@ -0,0 +1,43 @@
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace HealthyDashboard
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHealthChecks();
+            services.AddHealthChecksUI();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+
+            app.UseHealthChecks("/healthy",
+                new HealthCheckOptions
+                {
+                    Predicate = _ => true,
+                    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+                });
+            app.UseHealthChecksUI(opt =>
+            {
+                opt.UIPath = "/dashboard";
+            });
+        }
+    }
+}
